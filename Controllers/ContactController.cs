5b6e33a89e1dd60364e2a44f88dc0db91d5875a3
Muskan_Gupta <115883547+27082004muskan@users.noreply.github.com>
2026-09-05 17:07:30 +0530

    Add files via upload

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecondMVCProject.Models;
+
+namespace SecondMVCProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        //defining 2 fields to pass the email and address to he View
+        private readonly string email = "contact@beststore.com";
+        private readonly string address = "New York , USA";
+
+
+        public IActionResult Index()
+        {
+
+            //Passing email with View Data and address with ViewBag
+            ViewData["EmailAddress"] = email;
+            ViewBag.Address = address;
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(ContactDto model)
+        {
+
+            //Passing email with View Data and address with ViewBag
+            ViewData["EmailAddress"] = email;
+            ViewBag.Address = address;
+
+            if (!ModelState.IsValid) 
+            {
+                return View(model);
+            }
+
+            //store a contact data in the database
+            ViewBag.SuccessMessage = "Your message is recieved successfully";
+           
+            return View(model);
+        }
+    }
+}
